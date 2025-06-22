3ee49a5cd9efec1923332d377b71bd14b824a659
César Diego <diegoruelasflores@gmail.com>
2025-06-22 17:39:45 -0500

    New Changes

--- tests/conftest.py
+++
@@ -1,7 +0,0 @@
-import sys
-import os
-
-# Add the src directory to the Python path
-# This allows pytest to find modules in src/ (e.g., src.database.mongo_connection)
-# when tests are run from the root directory.
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
